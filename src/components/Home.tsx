d1c07f0094e0f2a0ebcdec78ceb81f68bced92f1
Kah Hong <kahhong.tai@gmail.com>
2024-12-18 00:28:25 +0800

    UI changes to homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,7 @@
 import { BiLogoTelegram } from 'react-icons/bi';
 import About from './About';
 import Contact from './Contact';
+
 function Home() {
     return (
         <main className='flex flex-col gap-28'>
@@ -15,17 +16,18 @@ function Home() {
                     alt='Diametric Labs'
                     className='h-48 w-auto dark:hidden object-contain'
                 />
-                <h1 className='text-4xl text-primary dark:text-dark-primary font-bold py-10 text-center'>
-                    Make decisions with Solaris for greater clarity and
-                    confidence
+                <h1 className='text-4xl text-primary dark:text-dark-primary font-bold py-10 text-center max-w-6xl '>
+                    We are conducting a large-scale experiment on AI-assisted
+                    personal decision-making.
                 </h1>
-                <p className='text-lg text-secondary dark:text-dark-secondary max-w-2xl text-center px-4'>
-                    Solaris, a multi-platform decision-making agent that
-                    operates across various messaging applications globally
+                <p className='text-lg text-secondary dark:text-dark-secondary max-w-4xl text-center px-4 mb-8'>
+                    To facilitate this experiment, we are building Solaris, a
+                    multi-platform decision-making agent that operates across
+                    various messaging applications globally.
                 </p>
                 <a href='https://t.me/diametricbot' target='_blank'>
-                    <button className='bg-accent text-primary dark:bg-dark-primary dark:text-dark-background rounded-lg px-4 py-2 flex items-center gap-2'>
-                        <BiLogoTelegram /> Get started for free on Telegram
+                    <button className='bg-accent text-primary dark:bg-dark-primary dark:text-dark-background rounded-lg px-6 py-3 flex items-center gap-3 text-lg'>
+                        <BiLogoTelegram className="text-xl" /> Get started for free on Telegram
                     </button>
                 </a>
             </section>
