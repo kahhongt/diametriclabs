2391062983e4b5d369d1891c2e29a4770c336a46
rachaeltay <25266371+rachaeltay@users.noreply.github.com>
2024-12-17 17:15:15 +0800

    Still broken export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/diametriclabs',
+    trailingSlash: true,
     images: {
         unoptimized: true,
     },
